28dd4a854d849827e87863b97b7da1a11568bad1
edvanchaves <edvanchaves@gmail.com>
2025-02-06 19:59:13 -0300

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,15 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Computação em Nuvem - 2023.2!\n');
+  res.send('Computação em Nuvem - 2025.1!\n');
 });
 
 app.get('/teste', function (req, res) {
   res.send('Testando rota de teste ...\n');
+});
+
+app.get('/teste2', function (req, res) {
+  res.send('Testando rota de teste2 ...\n');
 });
 
 app.listen(8080, function () {
