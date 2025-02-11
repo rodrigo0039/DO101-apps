e78788567b34f5926e289c6a852fe72ea9b6224d
edvanchaves <edvanchaves@gmail.com>
2025-02-11 20:02:37 -0300

    Update app.js

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Essa é a versão 14 desta aplicação (Usando WebHooks).' + '\n';
+    response = 'Essa é a versão 15 desta aplicação (Usando WebHooks).' + '\n';
 
     //send the response to the client
     res.send(response);
